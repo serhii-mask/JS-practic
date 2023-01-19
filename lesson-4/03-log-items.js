628f519192eee4d0d4a54e3b85894f6974cb2b63
Serhii <serhii.mask@gmail.com>
2023-01-19 10:33:26 +0200

    lesson-4 add task-2

---
+++ lesson-4/03-log-items.js
@@ -0,0 +1,9 @@
+const logItems = function (items) {
+	for (const item of items) {
+		console.log(item);
+	}
+};
+
+logItems(['Sofa', 'Afgust', 'Mark', 'Nastiy']);
+logItems([1, 2, 3, 4, 5,]);
+logItems(['Pasta', 'Pateto', 'Beef']);
