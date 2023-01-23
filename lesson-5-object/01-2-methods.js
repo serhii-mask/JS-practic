ed2d20d8f33163ab464c3fdccec268353374f330
Serhii <serhii.mask@gmail.com>
2023-01-23 13:39:24 +0200

    this

--- lesson-5-object/01-2-methods.js
+++ lesson-5-object/01-2-methods.js
@@ -1,14 +1,48 @@
 const playlist = {
    name: 'My top playlist',
    rating: 5,
-   tracks: ['trak-1', 'trak-2', 'trak-3'],
-   trackCount: 3,
-   getName: function () {
+   tracks: ['track-1', 'track-2', 'track-3'],
+   // getName: function () {
+   //    //  Метод об'єкта
+   //    console.log('Hello!');
+   // },
+
+   getName() {
       //  Метод об'єкта
       console.log('Hello!');
+   },
+
+   changeName(newName) {
+      console.log('Всередині чендж: ', this);
+      this.name = newName;
+   },
+
+   addTrack(track) {
+      this.tracks.push(track);
+   },
+
+   updateRating(newRating) {
+      this.rating = newRating;
+   },
+
+   getTrackCount() {
+      return this.tracks.length;
    },
 };
 
 console.log(playlist);
 
 playlist.getName();
+
+playlist.changeName('New name');
+console.log(playlist);
+
+console.log(playlist.getTrackCount());
+
+playlist.addTrack('track-4');
+console.log(playlist);
+
+playlist.updateRating(10);
+console.log(playlist);
+
+console.log(playlist.getTrackCount());
