49807290589c2d69bd7d33c24c75703b2befaba3
Serhii <serhii.mask@gmail.com>
2023-02-24 18:48:23 +0200

    finish lesson 13

--- lesson-13-event/06-modal.js
+++ lesson-13-event/06-modal.js
@@ -5,3 +5,33 @@ const refs = {
    closeModalBtn: document.querySelector('[data-action="close-modal"]'),
    backdrop: document.querySelector('.js-backdrop'),
 };
+
+refs.openModalBtn.addEventListener('click', onOpenModal);
+refs.closeModalBtn.addEventListener('click', onCloseModal);
+refs.backdrop.addEventListener('click', onBackdropClick);
+
+function onOpenModal() {
+   window.addEventListener('keydown', onEseKeyPress);
+   document.body.classList.add('show-modal');
+}
+
+function onCloseModal() {
+   window.removeEventListener('keydown', onEseKeyPress);
+   document.body.classList.remove('show-modal');
+}
+
+function onBackdropClick(event) {
+   console.log(event.currentTarget);
+   console.log(event.target);
+
+   if (event.currentTarget === event.target) {
+      onCloseModal();
+   }
+}
+
+function onEseKeyPress(event) {
+   console.log(event);
+   if (event.code === 'Escape') {
+      onCloseModal();
+   }
+}
