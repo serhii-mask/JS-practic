f1177d085c99677ace8853e4fe1bfce6c9a0e449
Serhii <serhii.mask@gmail.com>
2023-02-24 17:40:44 +0200

    modal

---
+++ lesson-13-event/06-modal.js
@@ -0,0 +1,7 @@
+// --------------------------
+
+const refs = {
+   openModalBtn: document.querySelector('[data-action="open-modal"]'),
+   closeModalBtn: document.querySelector('[data-action="close-modal"]'),
+   backdrop: document.querySelector('.js-backdrop'),
+};
